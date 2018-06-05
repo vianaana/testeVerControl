7755e8c0dcfed5107c0275dfb7f22bce171308d4
DocAdmin <docadmin@lab3710.local>
2018-06-05 22:23:37 +0100

    correct do bug x2

--- testeVerControl/ViewController.swift
+++ testeVerControl/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     }
     
     func teste1(){
-        print("Esta mensagem estava errada - agora sim, mensagem correta")
+        print("Teste 1")
     }
     
     func teste2(){
